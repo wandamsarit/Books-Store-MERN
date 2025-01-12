cb9774bb8095432f0b4fd89a3e47ec7160ebbc41
Sarit Wandam <saritwandam123@gmail.com>
2025-01-12 07:32:13 +0200

    Books-Store-MERN-Project

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555; 
+
+export const mongoDBURL = `mongodb+srv://saritwandam123:books-store-MERN@books-store-mern.gj8ki.mongodb.net/?retryWrites=true&w=majority&appName=Books-Store-MERN`
